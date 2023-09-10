11d1bc0483e5687632b8059f9cf553da6dea53c4
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-09-10 13:31:16 +0300

    fix: fix array.js solution

--- Exercises/9-array.js
+++ Exercises/9-array.js
@@ -3,12 +3,12 @@
 const phonebook = [
   { name: 'Marcus', phone: '+37529668890' },
   { name: 'Jack Hanma', phone: '9999999999' },
-  { name: 'Baki Hanma', phone: '00000000' }
+  { name: 'Baki Hanma', phone: '000000' }
 ];
 
 const findPhoneByName = (name) => {
   for (const item of phonebook) {
-    if (item.name === name) return item.phone;
+    if (item.name === name) item.phone;
   }
 };
 
