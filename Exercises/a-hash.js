cc3445a3814f5937e57794124abd14a67c644bb3
Valera Kozhevnikov <kozevnikovvalera@mail.ru>
2023-09-10 14:26:16 +0300

    finished all tasks

--- Exercises/a-hash.js
+++ Exercises/a-hash.js
@@ -1,7 +1,16 @@
 'use strict';
+const handbook = [
+  { name: 'Marcus', phone: '+37529668890' },
+  { name: 'Jack Hanma', phone: '9999999999' },
+  { name: 'Baki Hanma', phone: '0000000001' }
+];
 
-const phonebook = null;
+const phonebook = new Map();
 
-const findPhoneByName = null;
+handbook.forEach((item) => {
+  phonebook.set(item.name, item.phone);
+});
+
+const findPhoneByName = (name) => phonebook.get(name);
 
 module.exports = { phonebook, findPhoneByName };
